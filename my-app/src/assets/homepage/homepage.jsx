8f40525ceb2742a60fea5775ce7bc1cf185c8d5a
Esse <esefapohunda@Gmail.com>
2026-02-13 16:13:08 -0300

    fix: normalize homepage component imports for vercel build

--- my-app/src/assets/homepage/homepage.jsx
+++ my-app/src/assets/homepage/homepage.jsx
@@ -10,9 +10,9 @@
  */
 
 import React from 'react'
-import Hero from '../components/hero'
-import ProductList from '../components/productList'
-import Banner from '../components/banner'
+import Hero from '../components/hero.jsx'
+import ProductList from '../components/productList.jsx'
+import Banner from '../components/banner.jsx'
 
 
 const homepage = () => {
