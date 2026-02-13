4dd9898068bac99959dc0ff5083ffcdfc2963e13
Esse <esefapohunda@Gmail.com>
2026-02-13 15:50:11 -0300

    //

--- my-app/src/assets/homepage/homepage.jsx
+++ my-app/src/assets/homepage/homepage.jsx
@@ -10,7 +10,7 @@
  */
 
 import React from 'react'
-import Hero from '../components/Hero'
+import Hero from '../components/hero'
 import ProductList from '../components/productList'
 import Banner from '../components/banner.jsx'
 
